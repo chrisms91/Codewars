6c8a7614b0d286ff9c9e587b3796bc8cb33aa465
Christopher Kim <christopherkim@dhcp-v064-090.mobile.uci.edu>
2017-02-10 16:05:35 -0800

    added other solution

--- python/Vasya - Clerk.py
+++ python/Vasya - Clerk.py
@@ -37,5 +37,25 @@ def tickets(people):
                 return 'NO'
     return 'YES'
 
+'''
+Other Solution
+def tickets(people):
+  till = {100.0:0, 50.0:0, 25.0:0}
+
+  for paid in people:
+    till[paid] += 1
+    change = paid-25.0
+    
+    for bill in (50,25):
+      while (bill <= change and till[bill] > 0):
+        till[bill] -= 1
+        change -= bill
+
+    if change != 0:
+      return 'NO'
+        
+  return 'YES'
+'''
+
 test.assert_equals(tickets([25, 25, 50]), "YES")
 test.assert_equals(tickets([25, 100]), "NO")
